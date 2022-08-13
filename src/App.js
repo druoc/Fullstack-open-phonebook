6834c445272d3c3931dd03670128fc88a05dd8c2
Dave <d.ruocco1987@googlemail.com>
2022-08-13 13:56:10 +0100

    Set up project, state and basic state update functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Person from './components/Person';
 
-function App() {
+const App = () => {
+
+  //variables/state
+  const [persons, setPersons] = useState([
+    { id: 1,
+      name: 'Dave Dev'
+       }
+  ]);
+
+  const [newName, setNewName] = useState('new name');
+
+  //functions
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+    }
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input 
+          value={newName}
+          onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+      <ul>
+        {persons.map(person => 
+         <Person key={person.id} personName={person.name}/> )}
+      </ul>
+      </div>
     </div>
+    
   );
-}
+};
 
 export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Person = (props) => {
+    return (
+        <p>{props.personName}</p>
+    );
+};
+
+export default Person;
